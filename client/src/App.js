1051bbd1ace71327a8e5aaec9262a910dc34bf4d
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2022-11-11 16:38:29 +0800

    Event Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 import Home from './Pages/Home';
 import Dashboard from './Pages/Dashboard/Dashboard(User)';
-import Event from './Pages/Event';
-import EventList from './Pages/EventList';
+import Event from './Pages/Event/Event';
+import EventList from './Pages/Event/EventList.js';
 import Error404 from './Pages/Error404';
 
 import {Route, Routes} from 'react-router-dom';
@@ -13,7 +13,7 @@ function App() {
   return <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/dashboard" element={<Dashboard/>}/>
-    <Route path="/events" element={<EventList/>}/>
+    <Route path="/events" element={<Event/>}/>
     <Route path="/events/:id" element={<Event/>}/>
     <Route path="*" element={<Error404/>}/>
   </Routes>
