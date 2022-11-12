dc7401e836a366775532c4aee4f4ffa169ca4b52
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2022-11-12 14:54:31 +0800

    Event Page and Event Tabs implementation

--- client/src/Components/Events/EventCard.js
+++ client/src/Components/Events/EventCard.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import ImagePlaceHolder from './../../Assets/Images/placeholder/image_placeholder.jpg';
-import './../../Assets/Styles/Components/style-EventCard.scss'
+import './../../Assets/Styles/Components/style-EventCard.scss';
 
 function EventCard() {
   return (
@@ -11,14 +11,14 @@ function EventCard() {
                 <h6 className="EventTitle">Blockchain Technology Intro</h6>
                 {/* Add class DateDue if the date if near on happening */}
                 <p className='Date BodyText3'>Today</p>
-                <div className='Container_Participants'>
+                {/* <div className='Container_Participants'>
                     <div className='Wrapper_Participants'>
                         <img src={ImagePlaceHolder} alt=""/>
                         <img src={ImagePlaceHolder} alt=""/>
                         <img src={ImagePlaceHolder} alt=""/>
                     </div>
                     <p>+<span>400</span> Participating</p>
-                </div>
+                </div> */}
             </div>
         </a>
     </div>
--- client/src/Pages/Event/Event.js
+++ client/src/Pages/Event/Event.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {useParams} from 'react-router-dom';
 
 import './../../Assets/Styles/Page/style-DashboardUser.scss';
@@ -9,11 +9,15 @@ import HeaderNavigation from './../../Components/DashboardUserNavigation/HeaderN
 import CredentialTab from '../../Components/Credential/CredentialTab.js';
 import EventCard from '../../Components/Events/EventCard.js';
 
+import ImagePlaceHolder from './../../Assets/Images/placeholder/image_placeholder.jpg';
+import ImagePosterSample from './../../Assets/Images/placeholder/PosterSample.jpg'
+
 function Event() {
 
   //Use this id to pull event info from database
   const {id} = useParams()
-  
+  const [isOpenPanel_Events , setIsOpenPanel_Events] = useState("FeaturedEvents"); 
+ 
   return (
     <div id='DashboardHolder'>
       <div id="Navigation">
@@ -25,21 +29,54 @@ function Event() {
 
           <section id='Events'>
             <h4>Events</h4>
-            <div id="Navigatoin_Event">
-              <button id="FeaturedEvents" className="Selected">Featured Events</button>
-              <button id="JoinedEvents" >Joined Events</button>
-              <button id="ManageEvents">Manage Events</button>
+            <div className="Navigation_Event">
+              <button id="FeaturedEvents" className="Selected"  onClick={() => setIsOpenPanel_Events("FeaturedEvents")}>Featured Events</button>
+              <button id="JoinedEvents" onClick={() => setIsOpenPanel_Events("JoinedEvents")}>Joined Events</button>
+              <button id="ManageEvents" onClick={() => setIsOpenPanel_Events("ManageEvents")}>Manage Events</button>
             </div>
-            <div class="container">
-              <div class="Container_Event_SlideShow">Container_Event_SlideShow</div>
-              <div class="Container_Advertisement">Container_Advertisement</div>
-              <div class="Container_UpcomingEvents">Container_UpcomingEvents</div>
+            
+            <div id='Panel_Events'>
+              <Panel_Events open={isOpenPanel_Events}/>
             </div>
           </section>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
+function Panel_Events(props){
+  if(props.open === "FeaturedEvents"){
+    return (
+      <div id='Container_FeaturedEvents'>
+          <div id='Container_Ads_FeaturedEvents' >
+              <div id='Slider_FeaturedEvents'>
+                <a href="http://"><img src={ImagePosterSample} alt=""/></a>
+              </div>
+              <div id='Ads_FeaturedEvents'>
+                <span>Advertisement</span>
+              </div>
+          </div>
+          <div id='Container_Upcoming_FeaturedEvents'>
+              <EventCard/>
+              <EventCard/>
+              <EventCard/>
+              <EventCard/>
+          </div>
+      </div>
+    );
+  }else if(props.open === "JoinedEvents"){
+    return (
+      <div id='Container_JoinedEvents'>
+          JoinedEvents
+      </div>
+    );
+  }else if(props.open === "ManageEvents"){
+    return (
+      <div id='Container_ManageEvents'>
+          ManageEvents
+      </div>
+    );
+  }
+};
 export default Event;
