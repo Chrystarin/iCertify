3eeb8122c65249f6e46c6a3df46f86dda522bdf2
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2022-12-04 13:11:12 +0800

    Analytics (Members, Events, Accountant)

--- client/src/Pages/Dashboard/Dashboard(Admin).js
+++ client/src/Pages/Dashboard/Dashboard(Admin).js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './../../Assets/Styles/Page/style-DashboardUser.scss';
 
+
 import Navigation from '../../Components/DashboardAdminNavigation/DashboardNavigationAdmin';
-import HeaderNavigation from '../../Components/DashboardAdminNavigation/HeaderNavigationAdmin';
+import HeaderNavigation from '../../Components/DashboardUserNavigation/HeaderNavigation';
 
 import CredentialTab from '../../Components/Credential/CredentialTab.js';
 import EventCard from '../../Components/Events/EventCard.js';
-function Dashboard() {
-
+function Dashboard(props) {
+  const carInfo = { name: "Ford", model: "Mustang" };
   return (
     <div id='DashboardHolder'>
       <div id="Navigation">
         <Navigation />
       </div>
       <div id="Holder_Content">
-        <HeaderNavigation/>
+
+        <HeaderNavigation dropdown={carInfo}/>
         <div id="Content">
             
           
--- client/src/Pages/Event/EventAdmin.js
+++ client/src/Pages/Event/EventAdmin.js
@@ -3,13 +3,13 @@ import './../../Assets/Styles/Page/style-DashboardUser.scss';
 import './../../Assets/Styles/Page/style-EventAdmin.scss';
 
 import Navigation from '../../Components/DashboardAdminNavigation/DashboardNavigationAdmin';
-import HeaderNavigation from '../../Components/DashboardAdminNavigation/HeaderNavigationAdmin';
+import HeaderNavigation from '../../Components/DashboardUserNavigation/HeaderNavigation';
 import Analytics from '../../Components/Events/Analytics'
 import EventCard from '../../Components/Events/EventCard.js';
 
 
 function Dashboard() {
-    const [isOpenPanel_Events , setIsOpenPanel_Events] = useState("All"); 
+    const [isOpenPanel_Events , setIsOpenPanel_Events] = useState("All");
   return (
     <div id='DashboardHolder'>
       <div id="Navigation">
@@ -18,26 +18,29 @@ function Dashboard() {
       <div id="Holder_Content">
         <HeaderNavigation/>
         <div id="Content">
-            <section id='Events'>
-                <h4>Events</h4>
+            <div id='AdminDasboard'>
+                <section id='Events'>
+                    <h4>Events</h4>
+                    
+                    <div className='Navigation_Event'>
+                        <button className={(isOpenPanel_Events === "All")? "Selected":""} onClick={() => setIsOpenPanel_Events("All")}>All</button>
+                        <button className={(isOpenPanel_Events === "Ongoing")? "Selected":""} onClick={() => setIsOpenPanel_Events("Ongoing")}>Ongoing</button>
+                        <button className={(isOpenPanel_Events === "Upcoming")? "Selected":""} onClick={() => setIsOpenPanel_Events("Upcoming")}>Upcoming</button>
+                        <button className={(isOpenPanel_Events === "Done")? "Selected":""} onClick={() => setIsOpenPanel_Events("Done")}>Done</button>
+                        <button className={(isOpenPanel_Events === "Drafts")? "Selected":""} onClick={() => setIsOpenPanel_Events("Drafts")}> Drafts</button>
+
+                        <a id="AddEventBtn" href="/Admin/Event/Create" style={{float:"right", height:"100%"}}>Add Event</a>
+                        <div className='Container_Filer'>
+
+                        </div>
+                    </div>
+                    <div id='Panel_Events'>
+                        <Panel_Events open={isOpenPanel_Events}/>
+                    </div>
+                </section>
                 
-                <div className='Navigation_Event'>
-                    <button className={(isOpenPanel_Events === "All")? "Selected":""} onClick={() => setIsOpenPanel_Events("All")}>All</button>
-                    <button className={(isOpenPanel_Events === "Ongoing")? "Selected":""} onClick={() => setIsOpenPanel_Events("Ongoing")}>Ongoing</button>
-                    <button className={(isOpenPanel_Events === "Upcoming")? "Selected":""} onClick={() => setIsOpenPanel_Events("Upcoming")}>Upcoming</button>
-                    <button className={(isOpenPanel_Events === "Done")? "Selected":""} onClick={() => setIsOpenPanel_Events("Done")}>Done</button>
-                    <button className={(isOpenPanel_Events === "Drafts")? "Selected":""} onClick={() => setIsOpenPanel_Events("Drafts")}> Drafts</button>
-
-                    <a id="AddEventBtn" href="/Admin/Event/Create" style={{float:"right", height:"100%"}}>Add Event</a>
-                    <button id=""  style={{float:"right"}}>Filter</button>       
-                </div>
-                <div id='Panel_Events'>
-                    <Panel_Events open={isOpenPanel_Events}/>
-                </div>
-            </section>
-            
-            <Analytics/>
-
+                <Analytics/>
+            </div>
         </div>
       </div>
     </div>
