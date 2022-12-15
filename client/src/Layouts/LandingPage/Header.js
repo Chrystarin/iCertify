0d18d49b879311b75eed5736efa38f648b41c568
haroldcastillo <haroldcastillo272@gmail.com>
2022-12-15 20:57:46 +0800

    Header Update

---
+++ client/src/Layouts/LandingPage/Header.js
@@ -0,0 +1,31 @@
+import React,{useState} from 'react';
+import './../../Assets/Styles/Components/style-Navigation.scss';
+
+import WordMark from './../../Assets/Images/brand/Whitehorizontal.png'
+import ModalLogin from '../../Components/Login/Login.js';
+
+export default function Header() {
+    const [isOpen , setIsOpen] = useState(false); 
+    const [isOpenSignup , setIsOpenSignup] = useState(false); 
+  return (
+    <header>
+        <div id="Wrapper_Header">
+            <a href="index.js" id="Home_nav">
+                <img src={WordMark} alt=""/>   
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="#About">About</a></li>
+                    <li><a href="#LearnMore">Learn More!</a></li>
+                    <li><a href="#Login" onClick={() => setIsOpen(true)}>Login</a></li>
+                    <ModalLogin open={isOpen} onClose={() => setIsOpen(false)}/>
+                    <li><a href="Dashboard/Signup" id="GetStarted" onClick={() => setIsOpenSignup(true)}> Get Started</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+
+
