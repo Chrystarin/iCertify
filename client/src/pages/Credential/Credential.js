098aa25fc52070945f9fd03102d17d011b6a18b5
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2023-01-04 00:17:15 +0800

    Update Header_SideNavigation

--- client/src/pages/Credential/Credential.js
+++ client/src/pages/Credential/Credential.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React,{useState} from 'react';
+import './Credential.scss'
+
 import CredentialTab from '../../components/CredentialTab/CredentialTab.js';
 import Table from '../../components/Table/Table.js';
 
+
 function Credential() {
+  const [TabActive, setTabActive] = useState("Documents");
   return (
     <div id='Credential'>
       <section>
@@ -10,10 +14,19 @@ function Credential() {
           <h2 className='SectionTitle'>Credential</h2>
           <h5 className='SectionSubTitle'>Collection of your Certificates</h5>
         </div>
-        <CredentialTab Active="Documents"/>
-      </section>
-      <section>
-        <Table/>
+        <CredentialTab Active={TabActive} Setter={setTabActive}/>
+        <div id='CredentialVIewPanel'>
+          {(TabActive === "Documents")?<>
+            Documents
+            <Table />
+          </>
+          :
+          <>
+            Requests
+            <Table />
+          </>
+          }
+        </div>
       </section>
     </div>
   )
