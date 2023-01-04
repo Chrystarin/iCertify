b864ea8afa63364fc8226813a0270f931cf17af4
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2023-01-04 23:00:24 +0800

    update credential view

--- client/src/components/UserPanel/UserPanelInfo.js
+++ client/src/components/UserPanel/UserPanelInfo.js
@@ -6,7 +6,10 @@ function UserPanelInfo(props) {
   return (
     <div id='UserPanel'>
         <img src={props.Image}/>
-        <h6>{props.Title}</h6>
+        <div>
+          <h6>{props.Title}</h6>
+          <p className='BodyText3'>{props.SubTitle}</p>
+        </div>
     </div>
   )
 }
