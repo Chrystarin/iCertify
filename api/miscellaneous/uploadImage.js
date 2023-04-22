2d81dd8fdff5b54f16c6b71aeef10c3003ceb216
MeepyAngelo <jonangelollagas@gmail.com>
2023-04-22 16:07:56 +0800

    Updated uploadImage()
    - used refresh token instead of access token

--- api/miscellaneous/uploadImage.js
+++ api/miscellaneous/uploadImage.js
@@ -1,25 +1,54 @@
 const { Dropbox } = require('dropbox');
 
-const { filesUpload, sharingCreateSharedLinkWithSettings } = new Dropbox({
-	accessToken: DROPBOX_ACCESS_TOKEN
+const { DBX_KEY, DBX_SECRET, DBX_REFRESH } = process.env;
+
+const dropbox = new Dropbox({
+	clientId: DBX_KEY,
+	clientSecret: DBX_SECRET,
+	refreshToken: DBX_REFRESH
 });
 
-module.exports = async ({ date, name }, path) => {
-	// Upload file
+module.exports = async (image, path) => {
+	const { data, name } = image;
+
+    // Upload image
 	const {
 		result: { path_display }
-	} = await filesUpload({
+	} = await dropbox.filesUpload({
 		path: `/iCertify/${path}.${name.split('.')[1]}`,
 		contents: data
 	});
 
-	// Create sharable link
-	const {
-		result: { url }
-	} = await sharingCreateSharedLinkWithSettings({ path: path_display });
+	let url;
 
-	// Make url to image uri
-	url = url.replace('www.dropbox.com', 'dl.dropboxusercontent.com');
+	try {
+		// Create share link
+		({
+			result: { url }
+		} = await dropbox.sharingCreateSharedLinkWithSettings({
+			path: path_display
+		}));
+	} catch (error) {
+		const {
+			error: {
+				error: {
+					'.tag': tag,
+					shared_link_already_exists: { metadata }
+				}
+			}
+		} = error;
+
+		/**
+		 * Check if error is shared_link_already_exists
+         * If so, get the existing shared link
+         * Otherwise, throw the error
+		 */
+		if (tag === 'shared_link_already_exists') ({ url } = metadata);
+		else throw error;
+	}
+
+	// Make share link to image uri
+	url = url?.replace('www.dropbox.com', 'dl.dropboxusercontent.com');
 
 	return url;
 };
