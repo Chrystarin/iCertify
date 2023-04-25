2049224515ad736d22d4997ef9f88d4f449e874c
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-04-25 12:54:52 +0800

    Create InstitutionRequests.js

---
+++ client/src/pages/Requests/InstitutionRequests.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+import axiosInstance from '../../utils/axios';
+
+function InstitutionRequests() {
+
+	const [requests, setRequests] = useState();
+
+	// Excecutes on page load
+    useEffect(() => {
+        fetchDocumentRequests();
+    }, [])
+    
+    // Retrieves Document Requests
+    const fetchDocumentRequests = async () => {
+        await axiosInstance
+            .get(`requests`,{
+                params: {
+                    type: 'document'
+                }
+            })
+            .then((response) => { 
+                setRequests(response.data)
+                // console.log(response.data)
+            });
+    };
+
+    const ProcessRequest = async (request, action) => {
+        console.log(request)
+        console.log(action)
+        // try {
+        //     await axiosInstance.patch(
+        //         `requests`,
+        //         JSON.stringify({ 
+        //             requestId: request.requestId,
+        //             status: action
+        //         })
+        //     )
+        //     .then((res)=>{
+        //         alert("Document Added!")
+        //         console.log(res.data)
+        //     })
+        // } catch (err) {      
+        //     console.error(err.message);
+        // }
+    }
+
+	if (!requests) return <div>loading...</div>;
+
+	return (
+		<>
+            <h1>Pending Requests</h1>
+			{requests.length > 0 && requests.map((request) => {
+                if (request.status=='pending') {
+                    return (
+                        <div key={request.requestId}>
+                            <h3>{request.requestor.name.firstName + ' ' + request.requestor.name.lastName}</h3>
+                            <h5>{request.requestor.walletAddress}</h5>
+                            <h5>{request.details.docId}</h5>
+                            <button onClick={()=>ProcessRequest(request, 'approved')}>Approve</button>
+                            <button onClick={()=>ProcessRequest(request, 'declined')}>Decline</button>
+                        </div>
+                    );
+                }
+            })}
+
+            <h1>Paid Requests</h1>
+			{requests.length > 0 && requests.map((request) => {
+                if (request.status=='paid') {
+                    return (
+                        <div key={request.requestId}>
+                            <h3>{request.requestor.name.firstName + ' ' + request.requestor.name.lastName}</h3>
+                            <h5>{request.requestor.walletAddress}</h5>
+                            <h5>{request.details.docId}</h5>
+                            <button onClick={console.log("Verify: " + request.requestId)}>Verify</button>
+                            <button onClick={console.log("Decline: " + request.requestId)}>Decline</button>
+                        </div>
+                    );
+                }
+            })}
+
+            <h1>Verified Requests</h1>
+			{requests.length > 0 && requests.map((request) => {
+                if (request.status=='paid') {
+                    return (
+                        <div key={request.requestId}>
+                            <h3>{request.requestor.name.firstName + ' ' + request.requestor.name.lastName}</h3>
+                            <h5>{request.requestor.walletAddress}</h5>
+                            <h5>{request.details.docId}</h5>
+                            <button onClick={console.log("Verify: " + request.requestId)}>Verify</button>
+                            <button onClick={console.log("Decline: " + request.requestId)}>Decline</button>
+                        </div>
+                    );
+                }
+            })}
+		</>	
+	);
+}
+
+export default InstitutionRequests;
