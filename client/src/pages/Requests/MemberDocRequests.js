a3c6a5c53f968555a02ac6083e1532bd1676e533
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2023-05-06 04:59:28 +0800

    Request Status Update (Verified = Green, To Pay = Red, Verifying = Yellow)

--- client/src/pages/Requests/MemberDocRequests.js
+++ client/src/pages/Requests/MemberDocRequests.js
@@ -55,8 +55,8 @@ function MemberDocRequests(){
     const keys = ["pending","approved","declined","paid","verified","processing","cancelled","completed"];
 
     const filterData = (data) => {
-        setMyRequests(data.filter((item)=> [keys[0],keys[3]].some((key)=> item.status?.toString().toLowerCase().includes(key))))
-        setToPay(data.filter((item)=> item.status?.toString().toLowerCase().includes(keys[1])));
+        setMyRequests(data.filter((item)=> [keys[0]].some((key)=> item.status?.toString().toLowerCase().includes(key))))
+        setToPay(data.filter((item)=> [keys[1],keys[3]].some((key)=> item.status?.toString().toLowerCase().includes(key))));
         setToRecieve(data.filter((item)=> [keys[4],keys[5]].some((key)=> item.status?.toString().toLowerCase().includes(key))));
         setFailedtransactions(data.filter((item)=> [keys[2],keys[6]].some((key)=> item.status?.toString().toLowerCase().includes(key))))
     }
@@ -120,7 +120,7 @@ function MemberDocRequests(){
                         }
                         onClick={() => setStepper('myrequests')}
                     >
-                        My Requests ({(typeof myRequest.lenght ==="undefined") ?"0":myRequest.length})
+                        My Requests ({myRequest.length})
                     </Button>
                     <Button 
                         variant={
@@ -130,7 +130,7 @@ function MemberDocRequests(){
                     }
                     onClick={() => setStepper('topay')}
                     >
-                        To Pay  ({(typeof toPay.length ==="undefined") ?"0":toPay.length})
+                        To Pay  ({toPay.length})
                     </Button>
                     <Button 
                         variant={
@@ -140,7 +140,7 @@ function MemberDocRequests(){
                     }
                     onClick={() => setStepper('torecieve')}
                     >
-                        To Recieve ({(typeof toRecieve.length ==="undefined") ?"0":toRecieve.length})
+                        To Recieve ({toRecieve.length})
                     </Button>
                     <div className='Stepper__Cut'></div>
                     <Button 
@@ -151,7 +151,7 @@ function MemberDocRequests(){
                     }
                     onClick={() => setStepper('failedtransactions')}
                     >
-                        Failed Transactions ({(typeof failedtransactions.length ==="undefined") ?"0":failedtransactions.length})
+                        Failed Transactions ({failedtransactions.length})
                     </Button>
                 </div>
                 <div className='Container__Section'>
@@ -226,28 +226,11 @@ function MemberDocRequests(){
     )
 
     function RequestCard({data,type}){
-        const [status,setStatus] = useState("Failed");
         const [anchorElNote, setAnchorElNote] = React.useState(null);
         const openNote = Boolean(anchorElNote);
-        useEffect(()=> {
-            switch (type) {
-                case "myrequests"  :
-                    setStatus("Pending")
-                    break;
-                case "topay"  :
-                    setStatus("Payment")
-                    break;
-                case "torecieve"  :
-                    setStatus("Processing")
-                    break;
-                case "failedtransactions" :
-                    setStatus("Failed")
-                    break;
-                default:
-                    break;
-            }
-        },[])
-    
+        //  myrequests , topay , torecieve ,failedtransactions 
+        // const keys = ["pending","approved","declined","paid","verified","processing","cancelled","completed"];
+        
     
         return <>
             <div className='RequestCardUser'>
@@ -257,9 +240,49 @@ function MemberDocRequests(){
                 </div>
                 <div className='RequestCardUser__Body'>
                     <h5>{data.details.offeredDoc.title}</h5>
-                    <p className='BodyText3 RequestCardUser__Body__Status' id={status}>
+
+                    {data.status === "pending"? <>
+                        <p className='BodyText3 RequestCardUser__Body__Status' id="Pending">Verifying your request</p>
+                    </>
+                    :""}
+                    {data.status === "approved"? <>
+                        <p className='BodyText3 RequestCardUser__Body__Status' id="Payment">Ready for payment</p>
+                    </>
+                    :""}
+                    {data.status === "paid"? <>
+                        <p className='BodyText3 RequestCardUser__Body__Status' id="Verifying">Verifying your payment</p>
+                    </>
+                    :""}
+                    {data.status === "verified"? <>
+                        <p className='BodyText3 RequestCardUser__Body__Status' id="Processing">Document is processing</p>
+                    </>
+                    :""}
+                    {data.status === "declined"? <>
+                        <p className='BodyText3 RequestCardUser__Body__Status' id="Failed">Your request is declined</p>
+                    </>
+                    :""}
+                    {data.status === "cancelled"? <>
+                        <p className='BodyText3 RequestCardUser__Body__Status' id="Failed">Your Request is cancelled</p>
+                    </>
+                    :""}
+
+
+
+
+
+
+                    
+                    {/* {type === "topay"? <>
+                        
+                    </>
+                    :""} */}
+                    
+
+                    {/* <p className='BodyText3 RequestCardUser__Body__Status' id={status}>
                         {(data.status==='paid') ? "FOR VERIFICATION": (data.status).toUpperCase()}
-                    </p>
+                    </p> */}
+
+
                     <ul className='RequestCardUser__Body__MoreInfo'>
                         <li>
                             <EventIcon/>
@@ -271,43 +294,42 @@ function MemberDocRequests(){
                         </li>
                     </ul>
                 </div>
-                {status==="Processing"?<>
+
                 
+                
+                {type === "torecieve"?<></>:<></>}
+                {type === "failedtransactions"?<>
+                    <div className='RequestCardUser__Footer'>
+                        <Button 
+                            className='RequestCardUser__Footer__Note' 
+                            variant='contained' 
+                            onClick={(event) => {
+                                setAnchorElNote(event.currentTarget);
+                            }}
+                        >View Note
+                        </Button>
+                        <Menu
+                            id="basic-menu"
+                            anchorEl={anchorElNote}
+                            open={openNote}
+                            onClose={() => {
+                                setAnchorElNote(null);
+                            }}
+                        >
+                            <div id='SelectDocumentDropdown'>
+                                <h5 id='SelectDocumentDropdown__Title'>Note:</h5>
+                                <p>{(!data.details?.note) ? 'None' : data.details.note}</p>
+                            </div>
+                        </Menu>
+                    </div>
                 </>:<>
-                    {(status === "Failed")?<>
-                        <div className='RequestCardUser__Footer'>
-                            <Button 
-                                className='RequestCardUser__Footer__Note' 
-                                variant='contained' 
-                                onClick={(event) => {
-                                    setAnchorElNote(event.currentTarget);
-                                }}
-                            >View Note
-                            </Button>
-                            <Menu
-                                id="basic-menu"
-                                anchorEl={anchorElNote}
-                                open={openNote}
-                                onClose={() => {
-                                    setAnchorElNote(null);
-                                }}
-                            >
-                                <div id='SelectDocumentDropdown'>
-                                    <h5 id='SelectDocumentDropdown__Title'>Note:</h5>
-                                    <p>{(!data.details?.note) ? 'None' : data.details.note}</p>
-                                </div>
-                            </Menu>
-                        </div>
-                    </>
-                    :
-                    <>
+                    {data.status === "pending" || data.status === "approved" || data.status === "paid"?<>
                         <div className='RequestCardUser__Footer'>
                             <div className='RequestCardUser__Footer__Buttons'>
                                 {(data.status === "paid")?
                                     <Button 
-                                    
-                                    variant='contained' 
-                                    disabled
+                                        variant='contained' 
+                                        disabled
                                     >
                                         Cancel
                                     </Button>
@@ -321,13 +343,17 @@ function MemberDocRequests(){
                                     </Button>
                                 }
                                 
-                                {(status === "Payment")?<Button variant='contained' onClick={()=>navigate(`/requests/pay/${data.requestId}`)}>Pay</Button>:<></>}
-                                {(status === "Pending")?<Button variant='contained' disabled>Pay</Button>:<></>}
+                                {(data.status === "approved")?<Button variant='contained' onClick={()=>navigate(`/requests/pay/${data.requestId}`)}>Pay</Button>:<>
+                                    <Button variant='contained' disabled>Pay</Button>
+                                </>}
                             </div>
                         </div>
-                    </>
-                    }
-                </>}
+                    </>:<></>}
+                
+                
+                
+                </>
+                }
             </div>
         </>
     }
