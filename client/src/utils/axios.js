523f389bc9467a76782774ea36dde33ce1449649
Dianne Chrystalin Brandez <47180066+Chrystarin@users.noreply.github.com>
2023-05-17 22:50:22 +0800

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -5,6 +5,7 @@ const baseURL = process.env.REACT_APP_API_URL
 export default axios.create({
 	baseURL,
 	withCredentials: true,
+    crossDomain: true,
 	headers: {
 		"Content-Type": "application/json"
 	}
