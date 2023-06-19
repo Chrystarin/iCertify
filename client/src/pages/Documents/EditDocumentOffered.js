fd28d9cfe3d0dfd145481ef172e2055d7d2378e0
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2023-06-20 07:10:48 +0800

    Update

--- client/src/pages/Documents/EditDocumentOffered.js
+++ client/src/pages/Documents/EditDocumentOffered.js
@@ -181,7 +181,7 @@ function EditDocumentOffered() {
                                 id="outlined-adornment-amount"
                                 startAdornment={<InputAdornment position="start">₱</InputAdornment>}
                                 label="Amount"
-                                defaultValue={form.price.$numberDecimal}
+                                defaultValue={form.price.toString()}
                                 onChange={(e)=>updateForm({ price: e.target.number})}
                             />
                         </FormControl>
