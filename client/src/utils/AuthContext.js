641285af5ed238df8df806df8ec7860feb4f597a
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2023-06-20 05:53:24 +0800

    Update AuthContext.js

--- client/src/utils/AuthContext.js
+++ client/src/utils/AuthContext.js
@@ -57,7 +57,7 @@ function AuthProvider({ children }) {
                 ...openSnackBar,
                 open:true,
                 type:'error',
-                note:error.response.data.message,
+                note:error?.response?.data?.message ?? error.toString(),
             }));
         }
     };
