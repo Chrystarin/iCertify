330dabde8ee672eccc1202520c4ea924c693df2a
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2023-06-20 08:19:39 +0800

    Update ProtectedRoute.js

--- client/src/utils/ProtectedRoute.js
+++ client/src/utils/ProtectedRoute.js
@@ -4,7 +4,10 @@ import { useLocation, Navigate, Outlet } from "react-router-dom";
 const ProtectedRoute = ({allowedRole}) => {
     //Gets locally stored user
     const user = localStorage.getItem("user");
-    const type = JSON.parse(localStorage.getItem("user")).type;
+    let type = {}
+    if(user){
+        type = JSON.parse(localStorage.getItem("user")).type;
+    }
 
     return(
         // Checks if user exists, if yes proceeds to page, if not proceeds to login
