e21147f90ac125c1c7ab04018bec084b876b3605
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2023-06-23 01:20:45 +0800

    Update InstitutionDocRequests.js

--- client/src/pages/Requests/InstitutionDocRequests.js
+++ client/src/pages/Requests/InstitutionDocRequests.js
@@ -257,10 +257,10 @@ function InstitutionDocRequests() {
                                 >
                                     Select Multiple
 								</Button>
-								{/* {selectMultiple?
+								{selectMultiple?
                                     <Button variant='contained' onClick={()=>{alert(MultipleSelectValue)}}>Validate All</Button>:
 								    <Button variant='contained' href='/documents/requests/manual'>Generate Document</Button>
-                                } */}
+                                }
 							</div>
                             <div>
                                 {/* {selectMultipleValue.map((value)=>{
